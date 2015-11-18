c42b7f090e36d9688318de1351742fadfa186cec
dankernel <google@dankook.ac.kr>
2015-11-18 15:34:19 +0900

    auto-20151118-153419

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015년 11월 18일 15시 12분 58초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+int main(int argc, const char *argv[])
+{
+  
+  return 0;
+}
