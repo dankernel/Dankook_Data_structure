ca480b627d32a7e053e2d40ea84ff0f960c4438a
dankernel <google@dankook.ac.kr>
2015-11-27 19:55:40 +0900

    auto-20151127-195540

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <stdbool.h>
 
 #define nmax 1024
 
@@ -36,15 +37,27 @@ class Graph
     Graph(const int n = 0) { 
       // 이차원 배열의 동적 생성
       length = new int*[n];        
-      for(int i=0; i<n; i++) 
+      for(int i = 0; i < n; i++) 
         length[i] = new int [n];
+
+      // Allocation Max
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          length[j][i] = 0x7FFF;
+        }
+      }
+
       this->n = n;
+
+      dist = new int[n];
+      s = new bool[n];
     }; 
 
     void ShortestPath(const int);
     int choose(const int);
     int insert(int x, int y, int data);
     void print_array(void);
+    void print_dist(void);
 };
 
 /* 
@@ -56,6 +69,9 @@ int Graph::insert(int x, int y, int data)
   return data;
 };
 
+/*  
+ * Print array
+ */
 void Graph::print_array(void) 
 {
   int i, j, n = this->n;
@@ -63,7 +79,7 @@ void Graph::print_array(void)
   cout << "===== PRINT ===== \n";
   for (i = 0; i < n; i++) {
     for (j = 0; j < n; j++) {
-      printf("%4d", length[i][j]);
+      printf("%8d", length[i][j]);
     }
     cout << "\n";
   }
@@ -75,32 +91,68 @@ void Graph::print_array(void)
  * dist[j],0 ? j? n은 n개의 정점을 가진 방향 그래프 G에서 정점 v로부터 정점 j
  * 까지의 최단 경로 길이로 설정됨. 간선의 길이는 length[j][j]로 주어짐.
  */
-/*  
 void Graph::ShortestPath(const int v)
 {
+  int i = 0;
+
   // 초기화
-  for (int i=0; i<n; i++) {
-    s[i]=FALSE; dist[i]=length[v][i];
-  }  
-  
-  s[v] = TRUE;
+  for (int i = 0; i < n; i++) {
+    s[i] = false; 
+    dist[i] = length[v][i];
+  }
+ 
+  s[v] = true;
   dist[v] = 0;
 
   // 정점 v로부터 n-1개 경로를 결정
-  for (i=0; i<n-2; i++) {     
+  for (i = 0; i < n-1; i++) {     
   
     // choose는 dist[u] = minimum dist[w]인 u를 반환
     int u = choose(n); 
+    s[u] = true;
 
-    // (여기서 s[w]=FALSE) 
-    s[u] = TRUE;
-    for (int w=0; w<n; w++) {
-      if(!s[w])
-        if(dist[u] + length[u][w] < dist[w])
+    for (int w = 0; w < n; w++) {
+      if (!s[w]) {
+        if (dist[u] + length[u][w] < dist[w]) {
+          dist[w] = dist[u] + length[u][w];
+        }
+      }
     }
   }
-} 
-*/
+  
+}; 
+
+int Graph::choose(const int n)
+{
+  int min = 0x7FFF;
+  int min_index = 0;
+  int i = 0;
+
+  if (n < 0)
+    return -1;
+
+  for (i = 0; i < n; i++) {
+    if (dist[i] < min && s[i] == false) {
+      min = dist[i];
+      min_index = i;
+    }
+  }
+
+  return min_index;
+}
+
+void Graph::print_dist(void)
+{
+  int i;
+
+  for (i = 0; i < n; i++) {
+    cout << dist[i] << " ";
+  }
+
+  cout << "\n";
+
+}
+
 
 int main(int argc, const char *argv[])
 {
@@ -120,17 +172,18 @@ int main(int argc, const char *argv[])
     int x, y, data;
     cin >> x >> y >> data;
     graph->insert(x, y, data);
-    printf("array[%d][%d] : %d \n", y, x, data);
+    printf("array[%d][%d] : %d \n", x, y, data);
   }
 
-  /* ===== */
+  printf("End Insert \n");
 
+  /* ===== RUN ===== */
+  graph->print_array();
+  graph->ShortestPath(0);
+  graph->print_dist();
 
   /* End */
-  graph->print_array();
-  cout << "END!!";
+  cout << "END!! \n";
   return 0;
 }
-
-
 
